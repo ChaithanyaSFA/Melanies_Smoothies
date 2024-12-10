5f2a5a6f6a9a526be93f20c0e0cd50ab7db6b557
ChaithanyaSFA <chaithanya.susarla@gmail.com>
2024-12-10 19:04:42 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,5 +43,8 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         name = 'Your Smoothie is ordered,'  f'{name_on_order}!'
         st.success(name , icon="✅")
-
+#New block added for snoothifroot nuterition information
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response.json(), use_container_width=True)
 
